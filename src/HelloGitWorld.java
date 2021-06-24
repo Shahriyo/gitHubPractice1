41bd1eabf36a5a1546d707d2159cb6cf4b9a6675
Shak <70875959+Shahriyo@users.noreply.github.com>
2021-06-24 18:11:59 -0400

    add Login

--- src/HelloGitWorld.java
+++ src/HelloGitWorld.java
@@ -1,7 +1,11 @@
 public class HelloGitWorld {
     public static void main(String[] args) {
         System.out.println("Hello Github");
-
+/*
+git init ( initializes new repository for a project)
+git add . ( adds the code)
+git commit -m"add HelloGitWorld" --> adds the code to repository
+ */
     }
 
 }
---
+++ src/Tests/Login.java
@@ -0,0 +1,8 @@
+package Tests;
+
+public class Login {
+    public static void main(String[] args) {
+        //Shaks Code
+
+    }
+}
