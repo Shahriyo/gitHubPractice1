e031802d9a3e68bad69b50d007ff377f479412dc
Shak <70875959+Shahriyo@users.noreply.github.com>
2021-06-25 11:56:44 -0400

    .add Allgood

--- src/HelloGitWorld.java
+++ src/HelloGitWorld.java
@@ -5,6 +5,9 @@ public class HelloGitWorld {
 git init ( initializes new repository for a project)
 git add . ( adds the code)
 git commit -m"add HelloGitWorld" --> adds the code to repository
+git log shows the history of commit
+git status shows you where you at and what steps needs to be taken
+
  */
     }
 
---
+++ src/day1_GitHub/Allgood.java
@@ -0,0 +1,10 @@
+package day1_GitHub;
+
+public class Allgood {
+
+    public static void main(String[] args) {
+        // The weather is good
+
+
+    }
+}
