df70df64286b1adcd8a9e3f1be1b68992198cf0f
Shahriyo <70875959+Shahriyo@users.noreply.github.com>
2021-06-26 16:09:48 -0400

    Update Task2.java

--- src/day1_GitHub/Task2.java
+++ src/day1_GitHub/Task2.java
@@ -7,5 +7,10 @@ public class Task2 {
         today="Practice push and pull on github";
         int day = 2;
 
+        // trying to change the code
+        day=3;
+        //assigned day to day 3 to see the changes on git local
+        today= " Practice , pull on git local to see the changes made remotely" 
+            
     }
 }
