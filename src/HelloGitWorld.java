ac2aeba35189059f09d5a5976bb99f52aff79004
Shak <70875959+Shahriyo@users.noreply.github.com>
2021-06-26 16:05:19 -0400

    Add Task2

--- src/HelloGitWorld.java
+++ src/HelloGitWorld.java
@@ -7,7 +7,11 @@ git add . ( adds the code)
 git commit -m"add HelloGitWorld" --> adds the code to repository
 git log shows the history of commit
 git status shows you where you at and what steps needs to be taken
-
+git is open source version control system
+Github is website that allows you to create online repositories
+or upload your git repositories(storehouse, reservoir)
+Gives a visual interface
+git remote add origin https://github.come/your/vyrack
  */
     }
 
---
+++ src/day1_GitHub/Task2.java
@@ -0,0 +1,11 @@
+package day1_GitHub;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        String today;
+        today="Practice push and pull on github";
+        int day = 2;
+
+    }
+}
